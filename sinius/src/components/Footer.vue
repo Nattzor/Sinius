9e2176a9b0862b201e8ad661bca216d85575a060
Nattzor <74113960+Nattzor@users.noreply.github.com>
2022-02-21 08:42:36 +0100

    style fix, problems w fontawesom

--- sinius/src/components/Footer.vue
+++ sinius/src/components/Footer.vue
@@ -2,9 +2,9 @@
    <div class="footer">
   <p> ADDRESS
 PO Box 149, <br> Korumburra VIC. 3950 Australia </p>
-<p><b> support@sinus.se</b></p>
+<p><b> <font-awesome-icon icon="fa-solid fa-envelope" /> support@sinus.se</b></p>
 <p>80 dagars öppet köp </p>
-<p> Gratis retur* </p>
+<p> Gratis retur* <font-awesome-icon icon="fa-solid fa-truck" /></p>
 <p>Gratis Leverans* </p>
 <p> Betal metoder</p>
    </div>
@@ -17,6 +17,7 @@ export default {
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Work+Sans:wght@700&display=swap");
 .footer {
    display: grid;
    grid-template-columns: repeat(6, 1fr);
@@ -25,7 +26,8 @@ export default {
 }
 
 p {
-   font-size: 1.20rem;
+  font-family: "Work Sans", sans-serif;
+   font-size: 1rem;
    margin: 1rem;
 }
 
