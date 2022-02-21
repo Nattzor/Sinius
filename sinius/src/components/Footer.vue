83b6adc6259562b8361755c1f05b497429f7384a
Nattzor <74113960+Nattzor@users.noreply.github.com>
2022-02-21 08:18:43 +0100

    create footer

--- sinius/src/components/Footer.vue
+++ sinius/src/components/Footer.vue
@@ -1,6 +1,12 @@
 <template>
-   <div class="Footer">
-  <p> Hello iam ze Footer </p>
+   <div class="footer">
+  <p> ADDRESS
+PO Box 149, <br> Korumburra VIC. 3950 Australia </p>
+<p><b> support@sinus.se</b></p>
+<p>80 dagars öppet köp </p>
+<p> Gratis retur* </p>
+<p>Gratis Leverans* </p>
+<p> Betal metoder</p>
    </div>
 </template>
 
@@ -11,5 +17,16 @@ export default {
 </script>
 
 <style>
+.footer {
+   display: grid;
+   grid-template-columns: repeat(6, 1fr);
+   background-color: rgba(132, 176, 193, 1);
+  align-items: center;
+}
+
+p {
+   font-size: 1.20rem;
+   margin: 1rem;
+}
 
 </style>
