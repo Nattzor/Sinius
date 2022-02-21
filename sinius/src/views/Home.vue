b743b87b214bc7b53ea0daafeb93604917e88be6
Nattzor <74113960+Nattzor@users.noreply.github.com>
2022-02-21 15:01:23 +0100

    homepage setup

--- sinius/src/views/Home.vue
+++ sinius/src/views/Home.vue
@@ -1,12 +1,45 @@
 <template>
-  <div>
-    <h1>Sinus</h1>
+  <div class="hero"
+    v-bind:style="{ 'background-image': 'url(' + backgroundImage + ')' }"
+  >
+    <h1> Welocme to Swedens biggest online skateboard store! <br> We have everything you want and more </h1>
+    <button class="toShopBtn"><router-link class="toShopBtn" to="/Products"> CHECK OUT THE STORE</router-link> </button>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+    return {
+      backgroundImage: require("@/assets/hero.png"),
+    };
+  },
+};
 </script>
 
-<style>
+<style scoped>
+* {
+   font-family: "Work Sans", sans-serif;
+}
+.hero {
+  text-align: center;
+    background-size: 100%;
+    height: 40rem;
+   background-repeat: no-repeat;
+}
+
+h1 {
+  color: white;
+  text-align: center;
+  padding-top: 10rem;
+}
+
+.toShopBtn {
+  margin: 5rem;
+  color: white;
+  border-color: white;
+  padding: 1rem;
+  border-radius: 100px;
+  background-color: Transparent;
+}
 </style>
